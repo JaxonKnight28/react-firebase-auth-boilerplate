f239f2ef1e29ef0e224244cd7b0b6be45d5b7fa7
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2024-09-09 13:35:18 -0600

    takeout stuff

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGRdbaNtTCekrhjYSHFcXqSjyyj91hn78",
-  authDomain: "auth-practice-31eb3.firebaseapp.com",
-  projectId: "auth-practice-31eb3",
-  storageBucket: "auth-practice-31eb3.appspot.com",
-  messagingSenderId: "148409641055",
-  appId: "1:148409641055:web:e52072f9789266dac553ea",
-  measurementId: "G-L15MFN5MDK"
+
 };
 
 const app = initializeApp(firebaseConfig);
