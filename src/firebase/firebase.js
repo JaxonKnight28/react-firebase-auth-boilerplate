cf3dfc0ce6177592bcb2768fc9f7055a5f12bc49
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2024-09-09 13:17:48 -0600

    Update firebase.js
    add my info

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
-  };
+  apiKey: "AIzaSyCGRdbaNtTCekrhjYSHFcXqSjyyj91hn78",
+  authDomain: "auth-practice-31eb3.firebaseapp.com",
+  projectId: "auth-practice-31eb3",
+  storageBucket: "auth-practice-31eb3.appspot.com",
+  messagingSenderId: "148409641055",
+  appId: "1:148409641055:web:e52072f9789266dac553ea",
+  measurementId: "G-L15MFN5MDK"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
